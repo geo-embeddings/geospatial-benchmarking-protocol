ceff5de74b0c8c87868ec8a66a8dacf7da627899
Pete Gadomski <pete.gadomski@gmail.com>
2026-03-24 12:44:26 -0700

    feat: initial scaffolding

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Container, Heading, Text } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <Container maxW="container.lg" py={10}>
+      <Heading size="2xl">Geospatial Benchmarking Protocol</Heading>
+      <Text mt={4}>Welcome to the application.</Text>
+    </Container>
+  );
+}
+
+export default App;
